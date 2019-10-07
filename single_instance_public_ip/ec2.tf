5128cf64f0150d0429048a39d9028bf913776da2
Damien Michalosky <Damien.Michalosky@gmail.com>
2019-10-07 15:57:02 -0400

    Upgrade to Terraform v0.12

--- single_instance_public_ip/ec2.tf
+++ single_instance_public_ip/ec2.tf
@@ -15,13 +15,14 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_instance" "singleinstance" {
-  ami           = "${data.aws_ami.ubuntu.id}"
-  instance_type = "t2.micro"
-  subnet_id     = "${aws_subnet.subnet-singleinstance.id}"
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.subnet-singleinstance.id
   associate_public_ip_address = true
-  vpc_security_group_ids = ["${aws_security_group.sg_ssh.id}", "${aws_security_group.sg_ssh.id}"]
-  key_name   = "damien-personal-key"
-  user_data = "${file("./files/user_data.sh")}"  
+  vpc_security_group_ids      = [aws_security_group.sg_ssh.id, aws_security_group.sg_ssh.id]
+  key_name                    = "damien-personal-key"
+  user_data                   = file("./files/user_data.sh")
 
-  tags = "${local.common_tags}"
+  tags = local.common_tags
 }
+
--- single_instance_public_ip/outputs.tf
+++ single_instance_public_ip/outputs.tf
@@ -1,3 +1,4 @@
 output "public_ip_address" {
-  value = ["${aws_instance.singleinstance.*.public_ip}"]
+  value = [aws_instance.singleinstance.*.public_ip]
 }
+
